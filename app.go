692f22f5df360816e2e02826364c090ff4d2ecb2
yrolland <yann.rolland@iadvize.com>
2023-06-09 23:47:50 +0200

    wip

--- app.go
+++ app.go
@@ -477,7 +477,7 @@ func alphaBeta(currentState state, depth int, alpha int, beta int, myPlayerId in
 	})
 
 	// only keep the best actions
-	MAX_KEEP := 8
+	MAX_KEEP := 100
 	if len(possibleActions) > MAX_KEEP {
 		//debug(fmt.Sprintf("keeping only the N best actions (instead of %d)", len(actions)))
 		possibleActions = possibleActions[:MAX_KEEP]
