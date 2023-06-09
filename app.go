4897956b8ab9c19dac7a0168ee9f660e7e25a9ea
yrolland <yann.rolland@iadvize.com>
2023-06-10 01:18:23 +0200

    perfs

--- app.go
+++ app.go
@@ -542,7 +542,7 @@ func alphaBeta(currentState state, depth int, alpha int, beta int, myPlayerId in
 	possibleActions := getPossibleActions(currentState, playerId)
 
 	// only keep the best actions
-	MAX_KEEP := 100
+	MAX_KEEP := 30
 	if len(possibleActions) > MAX_KEEP {
 		// sort actions by remove tile distance to opponent
 		opponentPosition := currentState.playersPosition[1-playerId]
